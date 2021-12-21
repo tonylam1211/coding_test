1140eebedb149922c47ab110edb34048cf94585e
tonylam1211 <tonylam1211@gmail.com>
2021-12-21 14:44:37 +0800

    movie coding test

---
+++ source/index.js
@@ -0,0 +1,36 @@
+const api_url = 'https://jsonkeeper.com/b/WTPZ';
+let mydata = "No Details";
+let app = Vue.createApp({
+    data: function () {
+        return {
+            greeting: "Hello, world",
+            myname: "Tony",
+            movies: [],
+            inList: true
+        };
+    },
+    methods: {
+        async getData() {
+            await fetch(api_url).then(resp => resp.json()).then(fdata => {
+                console.log("fdata below: ", fdata);
+                mydata = fdata;
+                console.log("mydata ", mydata);
+            });
+        },
+        async printData() {
+            await this.getData();
+            this.movies = mydata;
+            console.log("movies ",this.movies);
+        },
+        getRoundrating(rating) {
+            return rating.toFixed(1);
+        },
+        switchView() {
+            this.inList = !(this.inList);
+        }
+    },
+    beforeMount() {
+        this.printData();
+    }
+
+}).mount("#app");
